5bd4506b47c99e8846db1164deda4909897b8a0d
garyhubley <gary.hubley@dal.ca>
2017-01-26 15:32:46 -0400

    Created solution

---
+++ LAARICP/main.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/* MAXIMUMS */
+const double SHEARMAX = 0.5;
+const double TRANSMAX = 50.0;
+const double ROTMAX = 5.0;
+const double CENROTMAX = 50.0;
+const double SCALEMAX = 1.5;
+const double SCALEMIN = 0.5;
+
+const int FRAME_SZ = 800;
+
+/* COLOURS */
+const cv::Scalar WHITE = cv::Scalar( 255, 255, 255 );
+const cv::Scalar BLACK = cv::Scalar( 0, 0, 0 );
+const cv::Scalar BLUE = cv::Scalar( 255, 0, 0 );
+const cv::Scalar GREEN = cv::Scalar( 0, 255, 0 );
+const cv::Scalar RED = cv::Scalar( 0, 0, 255 );
+
